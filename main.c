dac58a17d04ca793b38c6ed1f265606a153eefb8
Huseen <136820327+hmunye@users.noreply.github.com>
2025-01-01 15:17:06 -0500

    now printing errno in error messages. locking and unlocking file manually. reading and printing characters from file and closing file stream

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // argc => argument count
 // *argv[] => argument vector (array of char * pointers)
@@ -12,13 +14,36 @@ int main(int argc, char *argv[]) {
 
     // `fopen()` opens the file whose name is the string pointed to
     // by path and associates a stream with it. Opening with `r` (read) mode.
-    FILE *file = fopen(argv[1], "r");  // Return a FILE pointer, otherwise NULL
+    FILE *file = fopen(argv[1], "r");  // return a FILE pointer, otherwise NULL
 
-    // `file` will have a value of 0x0 if the file could not be found
+    // `file` will have a memory address of 0x0 if the file could not be found
     if (file == NULL) {
-        fprintf(stderr, "ERROR: failed to open file: %s\n", argv[1]);
+        fprintf(stderr, "ERROR: failed to open file %s: %s\n", argv[1],
+                // used to convert an error code stored in the global `errno`
+                // variable into a human-readable error message
+                strerror(errno));
         exit(1);
     }
 
-    printf("file address: %p\n", file);
+    char buffer;
+
+    // acquire an exclusive lock on file
+    // avoid the overhead of locking the object for each operation
+    flockfile(file);
+
+    while ((buffer = getc_unlocked(file)) != EOF) {
+        printf("%c", buffer);
+    }
+
+    // release the lock on file
+    funlockfile(file);
+
+    // clean up open file stream
+    if (fclose(file) == EOF) {  // return 0, otherwise EOF
+        fprintf(stderr, "ERROR: failed to close file %s: %s\n", argv[1],
+                strerror(errno));
+        exit(1);
+    }
+
+    return 0;
 }
